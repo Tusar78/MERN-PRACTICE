4b573813b77c12e8bd2cc7e30c5fb9794ad9b3aa
Tusar <tusarahmed141@gmail.com>
2024-11-06 01:37:11 +0600

    cOutung recounting

--- core-concept-01/react-app/src/App.jsx
+++ core-concept-01/react-app/src/App.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import Friend from './Friend/Friend'
+import React, { useState } from "react";
+import Friend from "./Friend/Friend";
 
 const App = () => {
   const friendsData = [
-    {id: 1, name: 'Aopo', age: 24, major: 'Web'},
-    {id: 2, name: 'Salman', age: 24, major: 'Graphics'},
-    {id: 3, name: 'Jannat', age: 24, major: 'UI/UX'},
-    {id: 4, name: 'Asha', age: 24, major: 'Networking'},
-  ]
+    { id: 1, name: "Aopo", age: 24, major: "Web" },
+    { id: 2, name: "Salman", age: 24, major: "Graphics" },
+    { id: 3, name: "Jannat", age: 24, major: "UI/UX" },
+    { id: 4, name: "Asha", age: 24, major: "Networking" },
+  ];
+
+  const [count, setCount] = useState(0);
+  const handleCount = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+
+  const handleReduce = () => {
+    setCount(count - 1)
+  }
   return (
     <>
-      <Friend friends={{id: 1, name: 'Tusar', age: 25, major: 'WebDev'}}></Friend>
-      {friendsData.map((friend, id) => <Friend friends={friend} key={id}></Friend>)}
-    </>
-  )
-}
+      <p>{count}</p>
+      <button onClick={handleCount}>Add</button>
+      <button onClick={handleReduce}>Reduce</button>
 
-export default App
+      <Friend
+        friends={{ id: 1, name: "Tusar", age: 25, major: "WebDev" }}
+      ></Friend>
+      {friendsData.map((friend, id) => (
+        <Friend friends={friend} key={id}></Friend>
+      ))}
+    </>
+  );
+};
+
+export default App;
--- core-concept-01/react-app/src/Friend/Friend.jsx
+++ core-concept-01/react-app/src/Friend/Friend.jsx
@@ -9,6 +9,7 @@ const Friend = ({friends: {name, age, major}}) => {
         <h2>Name: {name}</h2>
         <p>age: {age}</p>
         <p>Major: {major}</p>
+        <button onClick={() => alert(name)}>Show Name</button>
       </div>
     </>
   )
