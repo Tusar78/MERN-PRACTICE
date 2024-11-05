e2846b435b4f78745379d54fb16c1823e79b1e13
Tusar <tusarahmed141@gmail.com>
2024-11-05 12:59:34 +0600

    Exploring conditional rendering

--- core-concept-01/react-app/src/Todo.jsx
+++ core-concept-01/react-app/src/Todo.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 const Todo = ({task, isDone}) => {
-    console.log(task, isDone);
-    
-  if (isDone) {
-    return <li>Complete: {task}</li>
-  } else {
-    return <li>Work On: {task}</li>
-  }
+    return(
+        // isDone ? <li>Complete: {task}</li> : <li>Work On: {task}</li>
+        // <li>
+        //     {isDone ? 'Complete' : 'Workon'}
+        // </li>
+
+        // <li>{task} {isDone && ': Done'}</li>
+        <li>{task} {isDone || ': Do it'}</li>
+    )    
 }
 
 export default Todo
